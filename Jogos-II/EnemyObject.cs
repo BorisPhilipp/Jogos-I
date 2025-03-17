955ce36d14e3684967b31bd646fa669ba522e3c5
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-17 08:51:59 -0300

    adicionando um SriptableObject e um MonoBehaviour

--- Jogos-II/EnemyObject.cs
+++ Jogos-II/EnemyObject.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+//muito interessante isso, ele cria um menu com os atributos abaixo
+// e após o Enemy/, ele cria um sub-Menu.
+// no Unity, botao direito -> Create -> Enemy -> New Enemy.
+
+[CreateAssetMenu(fileName = "enemyAttributes", menuName = "Enemy/New Enemy")]
+public class EnemyObject : ScriptableObject
+{
+	public string name;
+	public float health;
+	public float speed;
+	public float attack;
+
+}
